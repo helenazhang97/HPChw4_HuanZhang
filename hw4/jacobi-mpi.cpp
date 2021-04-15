21671ea5e03ee39dc3dfef40b7bd7a5afcebf5cd
Huan Zhang <hz1994@log-1.nyu.cluster>
2021-04-14 22:55:22 -0400

    almost done

--- hw4/jacobi-mpi.cpp
+++ hw4/jacobi-mpi.cpp
@@ -7,15 +7,16 @@
 #include <math.h>
 #include <mpi.h>
 #include <string.h>
-
+#include <cstdlib>
 /* compuate global residual, assuming ghost values are updated */
 double compute_residual(double *lu, int lN, double invhsq){
-  int i;
+  int i,j;
   double tmp, gres = 0.0, lres = 0.0;
-
   for (i = 1; i <= lN; i++){
-    tmp = ((2.0*lu[i] - lu[i-1] - lu[i+1]) * invhsq - 1);
-    lres += tmp * tmp;
+	for(j=1;j<=lN;j++){
+    	tmp = ((4.0*lu[i+(lN+2)*j] - lu[i-1+(lN+2)*j] - lu[i+1+(lN+2)*j] - lu[i+(lN+2)*(j-1)] - lu[i+(lN+2)*(j+1)]) * invhsq - 1);
+    	lres += tmp * tmp;
+	}
   }
   /* use allreduce for convenience; a reduce would also be sufficient */
   MPI_Allreduce(&lres, &gres, 1, MPI_DOUBLE, MPI_SUM, MPI_COMM_WORLD);
@@ -37,14 +38,16 @@ int main(int argc, char * argv[]){
   MPI_Get_processor_name(processor_name, &name_len);
   printf("Rank %d/%d running on %s.\n", mpirank, p, processor_name);
 
-  //sscanf(argv[1], "%d", &N);
-  //sscanf(argv[2], "%d", &max_iters);
-  N=64;//the number of points in each dimension is as a power of 2 such that it can be easily split into sieces for 
+  sscanf(argv[1], "%d", &N);
+  sscanf(argv[2], "%d", &max_iters);
+ 
+  
+  //the number of points in each dimension is as a power of 2 such that it can be easily split into sieces for 
   //2 x 2, 4 x 4, 8 x 8 etc processors, namely p
   // and sqrt(p) should be a power of 2.
-  max_iters=10000;
+ 
   int dimp=int(sqrt(p));
-  printf("processor per dimension is sqrt(p) = %d",dimp);
+  if(mpirank==0){ printf("Processor per dimension is sqrt(p) = %d",dimp);}
 
   /* compute number of unknowns handled by each process */
   lN = N / p;
@@ -75,12 +78,13 @@ int main(int argc, char * argv[]){
   double h = 1.0 / (N + 1);
   double hsq = h * h;
   double invhsq = 1./hsq;
-  double gres, gres0, tol = 1e-5;
+  double gres, gres0, tol = 1e-6;
 
   /* initial residual */
   gres0 = compute_residual(lu, lN, invhsq);
   gres = gres0;
 
+  
   for (iter = 0; iter < max_iters && gres/gres0 > tol; iter++) {
     /* Jacobi step for local points */
     for (long i=1;i<=lN;i++){
@@ -91,8 +95,10 @@ int main(int argc, char * argv[]){
 
 
     /* communicate ghost values */
-    if ((mpirank+1)% dimp!=0) {
-	  printf("dimp=%d, mpirank=%d",dimp,mpirank);
+
+	
+	if ((mpirank+1)% dimp!=0) {
+	 // printf("dimp=%d, mpirank=%d",dimp,mpirank);
       /* If not the most right process, send/recv bdry values to the right */
 	  for (long j=0;j<lN;j++){
 	  	rsendbuff[j]=lunew[lN+(lN+2)*(j+1)];
@@ -103,8 +109,9 @@ int main(int argc, char * argv[]){
 	  	lunew[lN+1+(lN+2)*(j+1)]=rrecvbuff[j];
 	  }
     }
-    if ((mpirank)% dimp!=0) {
-      /* If not the first process, send/recv bdry values to the left */
+
+	if ((mpirank)% dimp!=0) {
+      // If not the first process, send/recv bdry values to the left 
    	  for (long j=0;j<lN;j++){
 	  	lsendbuff[j]=lunew[1+(lN+2)*(j+1)];
 	  }
@@ -116,24 +123,24 @@ int main(int argc, char * argv[]){
 	}
 
     if (mpirank<p-dimp) {
-      /* If not the upper process, send/recv bdry values to the above */
+      // If not the upper process, send/recv bdry values to the above 
    	  for (long j=0;j<lN;j++){
 	  	usendbuff[j]=lunew[j+1+(lN+2)*(lN)];
 	  }
-	  MPI_Send(usendbuff, lN, MPI_DOUBLE, mpirank+4, 126, MPI_COMM_WORLD);
-      MPI_Recv(urecvbuff, lN, MPI_DOUBLE, mpirank+4, 125, MPI_COMM_WORLD, &status2);
+	  MPI_Send(usendbuff, lN, MPI_DOUBLE, mpirank+dimp, 126, MPI_COMM_WORLD);
+      MPI_Recv(urecvbuff, lN, MPI_DOUBLE, mpirank+dimp, 125, MPI_COMM_WORLD, &status2);
 	  for (long j=0;j<lN;j++){
 	  	lunew[j+1+(lN+2)*(lN+1)]=urecvbuff[j];
 	  }
 	}
 
     if (mpirank>=dimp) {
-      /* If not the lower process, send/recv bdry values to the below */
+      // If not the lower process, send/recv bdry values to the below 
    	  for (long j=0;j<lN;j++){
 	  	bsendbuff[j]=lunew[j+1+(lN+2)*1];
 	  }
-	  MPI_Send(bsendbuff, lN, MPI_DOUBLE, mpirank-4, 125, MPI_COMM_WORLD);
-      MPI_Recv(brecvbuff, lN, MPI_DOUBLE, mpirank-4, 126, MPI_COMM_WORLD, &status3);
+	  MPI_Send(bsendbuff, lN, MPI_DOUBLE, mpirank-dimp, 125, MPI_COMM_WORLD);
+      MPI_Recv(brecvbuff, lN, MPI_DOUBLE, mpirank-dimp, 126, MPI_COMM_WORLD, &status3);
 	  for (long j=0;j<lN;j++){
 	  	lunew[j+1+(lN+2)*0]=brecvbuff[j];
 	  }
